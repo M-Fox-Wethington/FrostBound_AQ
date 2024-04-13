6980b5228476b0a7afcc03758e6eccccb8ccb9e2
Michael Wethington <wethington.michael@stonybrook.edu>
2024-04-13 10:58:56 -0400

    Updated the script to correctly process across the entire catalog, taking into account differnt sensor names

--- Scripts/hdf5-to-geotiff/SSMI/25km_ProcessSeaIceDataToGeoTIFF.R
+++ Scripts/hdf5-to-geotiff/SSMI/25km_ProcessSeaIceDataToGeoTIFF.R
@@ -1,47 +1,65 @@
-# Load required libraries
-library(terra)   # For spatial data analysis and raster manipulation
-library(sf)      # For handling spatial vector data (e.g., points, lines, polygons)
+library(terra)
+library(sf)
+
+# Directory and files setup
+input_dir <- "D:/Manuscripts_localData/FrostBound_AQ/Datasets/NASA_Bootstrap_sic/staged"
+output_dir <- "D:/Manuscripts_localData/FrostBound_AQ/Datasets/NASA_Bootstrap_sic/processed"
+
+# Initialize lists to store filenames that weren't successfully processed
+unprocessed_files <- list()
+files_without_icecon <- list()
 
 # Define a function to process a single NetCDF file
 process_netcdf_file <- function(nc_file, input_dir, output_dir) {
-  # Extract the date from the file path using regex, assuming YYYYMMDD format
-  date_pattern <- ".*_(\\d{4})(\\d{2})(\\d{2})_v\\d+\\.\\d+\\.nc$"
-  date <- sub(date_pattern, "\\1-\\2-\\3", basename(nc_file))
-  
-  # Read the sea ice concentration data from the NetCDF file
-  sic_raster <- rast(nc_file, subds="Sea_ice_concentration") # Adjust "Sea_ice_concentration" based on the actual variable name
-  
-  # Optionally, transform the projection if it's not already in the desired CRS
-  # sic_raster_transformed <- project(sic_raster, "+proj=stere +lat_0=-90 +lat_ts=-70 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
-  # Note: Uncomment and adjust the above line if a projection transformation is needed.
-  
-  # Define the output filename using the specified output directory and date
-  output_filename <- sprintf("%sNSIDC0079_SEAICE_PS_S25km_%s.tif", output_dir, gsub("-", "", date))
-  
-  # Save the GeoTIFF in the designated output directory
-  writeRaster(sic_raster, filename=output_filename, overwrite=TRUE)
-  
-  print(paste0("Raster:", output_filename, " exported"))
-  
-  # Clear memory of large objects no longer needed
-  rm(date, output_filename, sic_raster)
-  
-  # Explicitly call garbage collection to free up memory space
-  gc()
+  tryCatch({
+    r <- rast(nc_file)
+    
+    # Print all variable names for diagnostic purposes
+    print(names(r))
+    
+    # Find any variables that contain 'ICECON' in their name
+    available_vars <- grep("ICECON", names(r), value = TRUE)
+    
+    if (length(available_vars) > 0) {
+      sic_raster <- rast(nc_file, subds=available_vars[1])
+      
+      values(sic_raster)[values(sic_raster) == 1100] <- NA  # Missing data
+      values(sic_raster)[values(sic_raster) == 1200] <- NA  # Land or ice-shelf
+      
+      # Use the filename from the input for the output
+      output_filename <- gsub(input_dir, output_dir, nc_file)
+      output_filename <- gsub(".nc", ".tif", output_filename)
+      
+      # Save the raster data as a GeoTIFF
+      writeRaster(sic_raster, filename=output_filename, overwrite=TRUE)
+      print(paste0("Raster saved as: ", output_filename))
+    } else {
+      files_without_icecon <<- append(files_without_icecon, nc_file)
+      print(paste0("No ICECON data in ", nc_file))
+    }
+  }, error = function(e) {
+    unprocessed_files <<- append(unprocessed_files, nc_file)
+    print(paste0("Error processing ", nc_file, ": ", e$message))
+  })
 }
 
-# Directory containing NetCDF files
-input_dir <- "D:/Manuscripts_localData/FrostBound_AQ/Datasets/SSMI/tmp"
-# Single output directory for all TIFF files
-output_dir <- "D:/Manuscripts_localData/FrostBound_AQ/Datasets/SSMI/tmp/output/"
 
-# Ensure the output directory exists, if not, create it
+
+
+# Ensure the output directory exists
 if (!dir.exists(output_dir)) {
   dir.create(output_dir, recursive = TRUE)
 }
 
-# List all NetCDF files in the directory
+# Process each NetCDF file
 netcdf_files <- list.files(input_dir, pattern = "\\.nc$", full.names = TRUE)
-
-# Apply the processing function to each NetCDF file
 lapply(netcdf_files, function(file) process_netcdf_file(file, input_dir, output_dir))
+# After the lapply call
+unprocessed_files_df <- data.frame(files = unlist(unprocessed_files), stringsAsFactors = FALSE)
+files_without_icecon_df <- data.frame(files = unlist(files_without_icecon), stringsAsFactors = FALSE)
+
+# Write to CSV
+write.csv(unprocessed_files_df, file=paste0(output_dir, "unprocessed_files.csv"), row.names = FALSE)
+write.csv(files_without_icecon_df, file=paste0(output_dir, "files_without_icecon.csv"), row.names = FALSE)
+
+
