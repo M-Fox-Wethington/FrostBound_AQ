a4f5d11d569757a79fb85565256b6df05c6d3fee
Michael Wethington <wethington.michael@stonybrook.edu>
2024-04-18 10:56:37 -0400

    Commit for 12 km AMSR Unified Processing

--- Scripts/SeaIceConcentrationAnalysis/Calculate-Sea-Ice-Metrics_NASA_Bootstrap_25km.R
+++ Scripts/SeaIceConcentrationAnalysis/Calculate-Sea-Ice-Metrics_NASA_Bootstrap_25km.R
@@ -1,125 +1,69 @@
-# Define a function to calculate sea ice concentration (SIC) averages for a specified region and time period
-# using satellite images.
-# Args:
-#   ssmi_tif_dir: Directory containing TIFF files of SSMI data
-#   colony_buffer_shp_dir: Directory of the shapefile representing the buffer around a colony
-#   region: The region of interest as a string
-#   time_period: The specific time period (e.g., Annual, Q1, Q2, Q3, Q4) for analysis
-#   out_dir: Output directory for the resulting CSV file
+library(terra)
+library(doParallel)
 
-ssmi_SeaIceAverages <- function(ssmi_tif_dir, colony_buffer_shp_dir, region, time_period, out_dir){
+# Define the file path for the raster stack
+r_fp <- "D:/Manuscripts_localData/FrostBound_AQ/Datasets/NASA_Bootstrap_sic/raster-stack/NASA_Bootstrap_SIC_Time_Series.nc"
+
+# Load the raster stack outside of the parallel loop for main session setup
+raster_stack <- rast(r_fp)
+
+# Define your function to calculate mean and check for sea ice extent
+calculate_layer_stats <- function(layer_index, file_path) {
+  local_raster <- rast(file_path)  # Load raster within the function
+  layer_data <- local_raster[[layer_index]]
   
-  # Load necessary libraries for data manipulation, spatial analysis, and plotting
-  require(terra) # For spatial data analysis and raster manipulation
-  require(sf) # For handling vector data
-  require(data.table) # For efficient data manipulation
-  require(tidyverse) # For data manipulation and visualization
-  require(tibble) # For creating tibbles, a modern reimagining of data frames
-  require(stringr) # For string manipulation
-  require(ggplot2) # For data visualization
+  # Check if all values are NA
+  data_values <- values(layer_data)
+  mean_value <- NA
+  ice_extent <- NA
   
-  # Configure R to use fixed scientific notation for readability
-  options("scipen"= 100, "digits"=4)
-  
-  # Generate a list of all TIFF files within the specified directory
-  rast_list <- list.files(ssmi_tif_dir, 
-                          pattern='.tif$', 
-                          all.files= TRUE, 
-                          full.names=TRUE)
-  
-  # Read the shapefile representing the buffer zone around a colony
-  v <- vect(colony_buffer_shp_dir)
-  
-  # Create a raster stack from the list of TIFF files
-  r <- rast(rast_list)
-  
-  # Clip the raster to the extent of the buffer zone plus a small margin (0.01 degrees)
-  crop <- terra::crop(r, ext(v) + .01)
-  
-  # Mask the clipped raster with the buffer zone to isolate the area of interest
-  mask <- mask(crop, v)
-  
-  # Convert the pixel values to represent SIC percentages (original scale is 0-1000)
-  data.SIC <- (mask / 10) 
-  
-  # Calculate the global mean SIC, removing NA values
-  data.SIC.frame <-  as.data.frame(terra::global(data.SIC, 
-                                                 fun = mean, 
-                                                 na.rm=TRUE))
-  
-  # Convert row names to a separate column for further manipulation
-  data.SIC.frame <- tibble::rownames_to_column(data.SIC.frame, "rID")
-  
-  # Duplicate the rID column to extract date information
-  data.SIC.frame$Date <- data.SIC.frame$rID
-  
-  # Extract the date substring and format it for readability
-  data.SIC.frame <- data.SIC.frame %>%
-    mutate(Date = substr(Date, 3, 8)) %>% 
-    rename(Mean_SIC = mean)
-  
-  # Reformat the date to include a dash for better readability
-  data.SIC.frame$Date <- sub("(.{4})(.*)", "\\1-\\2", data.SIC.frame$Date)
-  
-  # Split the formatted date into separate Year and Month columns
-  data.SIC.frame <- data.SIC.frame %>% separate(Date, c('Year', 'Month'))
-  
-  # Calculate average SIC based on the specified time period (Annual, Q1, Q2, Q3, Q4)
-  # and group data accordingly for analysis
-  if(time_period == "Annual")
-  { 
-    season <- "Annual" 
-    data.SIC.frame <- data.SIC.frame %>%
-      group_by(Year) %>%
-      summarize(GlobalMean = mean(Mean_SIC, na.rm=TRUE)) 
+  if (!all(is.na(data_values))) {
+    # Calculate mean excluding NA values
+    mean_value <- mean(data_values, na.rm = TRUE)
+    
+    # Calculate sea ice extent
+    # Convert sea ice concentration to binary (1 if >= 0.15, 0 otherwise)
+    binary_ice_values <- ifelse(data_values >= 0.15, 1, 0)
+    binary_ice_raster <- layer_data
+    values(binary_ice_raster) <- binary_ice_values
+    
+    # Calculate total ice extent by summing the areas of cells marked as ice
+    cell_area = 156.25  # Area of each cell in square kilometers
+    ice_extent <- sum(values(binary_ice_raster) * cell_area, na.rm = TRUE)
   }
   
-  # Repeat the process for each quarter, filtering by the respective months
-  # This approach isolates the data relevant to each season before calculating the average SIC
-  if(time_period == "Q1") { 
-    season <- "Q1" 
-    # Filter for Q1 months (January, February, March)
-    data.SIC.frame <- data.SIC.frame %>%
-      filter(Month %in% c("01", "02", "03")) %>%
-      group_by(Year) %>%
-      summarize(GrandMean = mean(Mean_SIC, na.rm=TRUE)) 
-  }
-  
-  # Similar filtering and summarization for Q2, Q3, and Q4
-  if(time_period == "Q2"){  
-    season <- "Q2" 
-    data.SIC.frame <- data.SIC.frame %>%
-      filter(Month %in% c("04", "05", "06")) %>%
-      group_by(Year) %>%
-      summarize(GrandMean = mean(Mean_SIC, na.rm=TRUE))
-  }
-  
-  if(time_period == "Q3"){  
-    season <- "Q3" 
-    data.SIC.frame <- data.SIC.frame %>%
-      filter(Month %in% c("07", "08", "09")) %>%
-      group_by(Year) %>%
-      summarize(GrandMean = mean(Mean_SIC, na.rm=TRUE))
-  }
-  
-  if(time_period == "Q4"){  
-    season <- "Q4" 
-    data.SIC.frame <- data.SIC.frame %>%
-      filter(Month %in% c("10", "11", "12")) %>%
-      group_by(Year) %>%
-      summarize(GrandMean = mean(Mean_SIC, na.rm=TRUE))
-  }
-  
-  # Optionally, view the head of the dataframe to inspect the first few rows
-  # This line can be commented out or removed based on preference
-  head(data.SIC.frame)
-  
-  # Construct the output file name dynamically based on the region and season
-  output_file_name <- paste0(out_dir, "/", region, "_", season, "_Mean_SIC_1979-2021.csv")
-  
-  # Export the data frame to a CSV file in the specified output directory
-  write.csv(data.SIC.frame, output_file_name, row.names = FALSE)
-  
-  # Print a message to indicate successful completion and the location of the output file
-  message("Data exported successfully to ", output_file_name)
+  return(list(mean = mean_value, ice_extent = ice_extent))
 }
+
+# Set up parallel computing
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+clusterExport(cl, varlist = c("calculate_layer_stats", "r_fp"))
+
+# Extract dates for each layer
+dates <- as.Date(time(raster_stack))
+
+# Perform calculations in parallel
+results <- foreach(i = 1:nlyr(raster_stack), .packages = 'terra') %dopar% {
+  calculate_layer_stats(i, r_fp)
+}
+
+# Stop the cluster after the task is done to free up resources
+stopCluster(cl)
+
+# Create a dataframe with results and additional data
+result_df <- data.frame(
+  Index = 1:nlyr(raster_stack),
+  Date = dates,
+  MeanSIC = sapply(results, `[[`, "mean"),
+  IceExtent = sapply(results, `[[`, "ice_extent")
+)
+
+# Save results to CSV
+write.csv(result_df, "sea_ice_data.csv", row.names = FALSE)
+
+# Save results to RDS
+saveRDS(result_df, "sea_ice_data.rds")
+
+# Display some of the results for verification
+print(head(result_df))
---
+++ Scripts/hdf5-to-geotiff/12km_ProcessSeaIceDataToGeoTIFF.R
@@ -0,0 +1,74 @@
+# Load required libraries
+library(terra)   # For spatial data analysis and raster manipulation
+library(rhdf5)   # For reading HDF5 files
+library(sf)      # For handling spatial vector data
+
+# Ensure output directory exists
+# output_dir <- "D:/D-Downloads/AMSRE-Unified_Catalog/processed"
+
+output_dir <-  "D:/Manuscripts_localData/FrostBound_AQ/Datasets/AMSR-Data/Processed"
+
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+
+# Define a function to process a single HDF5 file
+process_hdf5_file <- function(h5_file, input_dir, output_dir) {
+  # Extract the date from the file path using regex, assuming YYYYMMDD format before ".he5"
+  date_pattern <- ".*(\\d{4})(\\d{2})(\\d{2})\\.he5$"
+  date <- sub(date_pattern, "\\1-\\2-\\3", basename(h5_file))
+  
+  # Read Sea Ice Concentration data and apply scale factor
+  SeaIce_scaled <- h5read(h5_file, "/HDFEOS/GRIDS/SpPolarGrid12km/Data Fields/SI_12km_SH_ICECON_DAY")
+
+  
+    # Set value ranges based on documentation
+  SeaIce_scaled[SeaIce_scaled == 110] <- NA  # Missing data
+  SeaIce_scaled[SeaIce_scaled == 120] <- NA  # Land
+
+  # Read spatial dimension data for georeferencing
+  lat <- h5read(h5_file, "/HDFEOS/GRIDS/SpPolarGrid12km/lat")
+  lon <- h5read(h5_file, "/HDFEOS/GRIDS/SpPolarGrid12km/lon")
+  
+  # Flatten the matrices to vectors
+  SeaIce_vec <- as.vector(SeaIce_scaled)
+  lat_vec <- as.vector(lat)
+  lon_vec <- as.vector(lon)
+  
+  # Combine vectors into a data frame and convert to an 'sf' object
+  sic_points <- data.frame(lon = lon_vec, lat = lat_vec, SIC = SeaIce_vec)
+  sic_points_sf <- st_as_sf(sic_points, coords = c("lon", "lat"), crs = 4326, agr = "constant")
+  
+  # Transform geographic coordinates to the Antarctic Polar Stereographic South projection (EPSG:3412)
+  sic_points_sf_transformed <- st_transform(sic_points_sf, crs = "EPSG:3412")
+  
+  # Create a raster template with dimensions and extent matching the spatial points
+  r_corrected <- rast(nrows=664, ncols=632, xmin=-3950000, xmax=3950000, ymin=-3950000, ymax=4350000, crs="EPSG:3412")
+  
+  # Rasterize the transformed point data onto the raster template
+  r_sic <- rasterize(sic_points_sf_transformed, r_corrected, field="SIC", fun=mean)
+  
+  # Define the output filename using the output directory and date
+  output_filename <- paste0(output_dir, "/NSIDC-AU_SI12_SeaIce_12km_", gsub("-", "", date), ".tif")
+  
+  # Save the GeoTIFF in the output directory
+  writeRaster(r_sic, filename=output_filename, overwrite = TRUE)
+  
+  print(paste0("Raster:", output_filename, " exported"))
+  
+  # Clear memory of large objects no longer needed
+  rm(date, date_pattern, r_sic, sic_points, sic_points_sf, sic_points_sf_transformed, SeaIce_scaled, SeaIce_vec, lat, lat_vec, lon, lon_vec)
+  
+  # Explicitly call garbage collection to free up memory space
+  gc()
+}
+
+# Directory containing HDF5 files
+# input_dir <- "D:/D-Downloads/AMSRE-Unified_Catalog/staged"
+input_dir <- "D:/Manuscripts_localData/FrostBound_AQ/Datasets/AMSR-Data/Staged"
+
+# List all HDF5 files in the directory
+hdf5_files <- list.files(input_dir, pattern = "\\.he5$", full.names = TRUE)
+
+# Apply the processing function to each HDF5 file
+lapply(hdf5_files, function(file) process_hdf5_file(file, input_dir, output_dir))
